91c102d1d41e30e223cb10dea1d56a5dcc12fc3b
Miguel Salinas Gancedo <masalinas.gancedo@gmail.com>
2020-09-16 10:16:21 +0200

    last commit

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 # Description
-GSCC PoC Portal with these modules:
-- GSBI
-- GSPL
+Modular GSSC PoC using symbolic links
 
 # Add Modules soft links from src/app
+Create symbolic links in the paretn project
+
 ```bash
 ln -s ../../../gscc-poc-cp-gsbi/src gsbi
 ln -s ../../../gscc-poc-cp-gspl/src gspl
 ```
 
 # Lazy load soft links modules from app-routing.module.ts
+Add Lazy load routing for the modules included
 ```javascript
 ...
   { path: 'gsbi', loadChildren: () => import('./gsbi/gsbi.module').then(m => m.GsbiModule) },
@@ -18,6 +19,8 @@ ln -s ../../../gscc-poc-cp-gspl/src gspl
 ```
 
 # Modify build options from angular.json to preserve soft links compilation
+Add preserveSymlinks in angular.js file to compile angular project
+
 ```javascript
     "preserveSymlinks": true,
 ```
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,11 +8,18 @@ const routes: Routes = [
   //{ path: 'first-component', component: FirstComponent },
   { path: 'first-component', loadChildren: () => import('./first/first.module').then(m => m.FirstModule) },
   { path: 'second-component', component: SecondComponent },  
+
+  
   //{ path: 'gsbi', component: GsbiComponent },
+
   //{ path: 'gsbi', loadChildren: () => import('@masalinas/gsbi').then(m => m.GsbiModule) },  
   //{ path: 'gspl', loadChildren: () => import('@masalinas/gspl').then(m => m.GsplModule) },
+  
   { path: 'gsbi', loadChildren: () => import('./gsbi/gsbi.module').then(m => m.GsbiModule) },
-  { path: 'gspl', loadChildren: () => import('./gspl/gspl.module').then(m => m.GsplModule) },  
+  { path: 'gspl', loadChildren: () => import('./gspl/gspl.module').then(m => m.GsplModule) },
+
+  //{ path: 'gsbi', loadChildren: () => import('gscc-poc-cp-gsbi/src/gsbi.module').then(m => m.GsbiModule) },  
+  //{ path: 'gspl', loadChildren: () => import('gscc-poc-cp-gspl/src/gspl.module').then(m => m.GsplModule) },  
 ];
 
 @NgModule({
